a3b79ca6123b3551a5cbe03e9777236a2d020a9f
Nicky <nmguantai@gmail.com>
2021-04-12 20:04:20 +0300

    added users

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # custom apps
     'blog',
-    'users',
     # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
