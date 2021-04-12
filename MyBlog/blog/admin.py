ffa0ffffe5957183c51b355e3917b3505937fd1a
Nicky <nmguantai@gmail.com>
2021-04-12 16:51:25 +0300

    register the comment model to the admin

--- MyBlog/blog/admin.py
+++ MyBlog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, BlogPost
+from .models import Category, BlogPost, Comment
 
 
 # Register your models here.
@@ -10,6 +10,16 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'post']
     prepopulated_fields = {'slug': ('title',)}
 
-
 admin.site.register(Category)
 admin.site.register(BlogPost, PostAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
