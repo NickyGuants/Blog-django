8896b54bdf9ae82039f3d0cc6c574ae0bfc3f19a
Nicky <nmguantai@gmail.com>
2021-04-12 16:45:44 +0300

    Added the comment model

--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -41,3 +41,17 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post=models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    body=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+    active=models.BooleanField(default=False)
+
+    class Meta:
+        ordering= ['created_on']
+
+    def __str__(self):
+        return 'Cooment {} by {}'. format(self.body,self.name)
