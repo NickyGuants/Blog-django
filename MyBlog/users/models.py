bcfff198be414dd57ad169aa4ae483ed85a94d60
NickyGuants <nicholasguantai528@gmail.com>
2021-04-18 12:38:55 +0300

    update user model

--- MyBlog/users/models.py
+++ MyBlog/users/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user= models.OneToOneField(User, on_delete=models.CASCADE)
-    bio=models.TextField()
+    bio=models.TextField(max_length=200)
     image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+    interest= models.TextField(max_length=100)
 
     def __str__(self):
         return f'{self.user.username} Profile'
